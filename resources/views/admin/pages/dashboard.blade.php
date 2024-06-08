221352d01f8002c7d477b8f7ca8b11b716358375
Shethil <shithelahammed@gmail.com>
2024-06-08 21:40:30 +0600

    layout setup

---
+++ resources/views/admin/pages/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('admin.layouts.master')
+@section('page_title', 'Dashboard')
+
+@push('admin_style')
+@endpush
+
+@section('admin_content')
+    <h1>Welcome SA404</h1>
+@endsection
+
+@push('admin_script')
+@endpush
