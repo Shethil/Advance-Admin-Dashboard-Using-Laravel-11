e784ecf287cd91bc6cbea3cb59b6e44e3e9be40a
Shethil <shithelahammed@gmail.com>
2024-06-09 19:58:28 +0600

    module, permission, role, page model and migration added

---
+++ database/migrations/2024_06_08_154521_create_roles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('role_name');
+            $table->string('role_slug');
+            $table->text('role_note')->nullable()->comment('describes the role');
+            $table->boolean('is_deleteable')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
