f6f64f0a6832f23d07843e47c29219cb688f5b94
rizwan-gfx <rizwanalam1192@gmail.com>
2024-08-17 23:49:09 +0600

    admin dashboard data display

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Gate;
+use App\Models\Module;
+use App\Models\Page;
+use App\Models\Role;
+use App\Models\User;
 
 //
 
@@ -11,6 +15,18 @@ class HomeController extends Controller
     public function index()
     {
         Gate::authorize('access-dashboard');
-        return view('admin.pages.dashboard');
+        $user_count = User::count();
+        $role_count = Role::count();
+        $page_count = Page::count();
+        $module_count = Module::count();
+        $users = User::with(['role'])->latest('id')->paginate();
+
+        return view('admin.pages.dashboard', compact(
+            'user_count',
+            'role_count',
+            'page_count',
+            'module_count',
+            'users'));
+
     }
 }
