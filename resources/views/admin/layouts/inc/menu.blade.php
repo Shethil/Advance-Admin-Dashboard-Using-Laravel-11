e28d47bd62be745e5b7f0f9e25acba9a3884f91f
rizwan-gfx <rizwanalam1192@gmail.com>
2024-08-18 23:56:04 +0600

    Sidebar slide issue fixed

--- resources/views/admin/layouts/inc/menu.blade.php
+++ resources/views/admin/layouts/inc/menu.blade.php
@@ -173,50 +173,49 @@
             </li>
         @endcan
         @can('index-backup')
-        <li class="menu-item">
-            <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bx-data"></i>
-                <div data-i18n="Account Settings">Backup Management</div>
-            </a>
-            <ul class="menu-sub">
-                <li class="menu-item">
-                    <a href="{{ route('backup.index') }}" class="menu-link">
-                        <div data-i18n="Account">List</div>
-                    </a>
-                </li>
-            </ul>
-        </li>
+            <li class="menu-item">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bx-data"></i>
+                    <div data-i18n="Account Settings">Backup Management</div>
+                </a>
+                <ul class="menu-sub">
+                    <li class="menu-item">
+                        <a href="{{ route('backup.index') }}" class="menu-link">
+                            <div data-i18n="Account">List</div>
+                        </a>
+                    </li>
+                </ul>
+            </li>
         @endcan
-
         @can('general-setting-view')
-        <li class="menu-item">
-            <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bx-cog"></i>
-                <div data-i18n="Account Settings">System Setting Management</div>
-            </a>
-            <ul class="menu-sub">
-                <li class="menu-item">
-                    <a href="{{ route('settings.general') }}" class="menu-link">
-                        <div data-i18n="Account">General Setting</div>
-                    </a>
-                </li>
-                <li class="menu-item">
-                    <a href="{{ route('settings.apperance') }}" class="menu-link">
-                        <div data-i18n="Account">Apperance Setting</div>
-                    </a>
-                </li>
-                <li class="menu-item">
-                    <a href="{{ route('settings.mail') }}" class="menu-link">
-                        <div data-i18n="Account">Mail Setting</div>
-                    </a>
-                </li>
-                <li class="menu-item">
-                    <a href="{{ route('settings.socialite') }}" class="menu-link">
-                        <div data-i18n="Account">Socialite Setting</div>
-                    </a>
-                </li>
-            </ul>
-        </li>
+            <li class="menu-item">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bx-cog"></i>
+                    <div data-i18n="Account Settings">System Setting Management</div>
+                </a>
+                <ul class="menu-sub">
+                    <li class="menu-item">
+                        <a href="{{ route('settings.general') }}" class="menu-link">
+                            <div data-i18n="Account">General Setting</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="{{ route('settings.apperance') }}" class="menu-link">
+                            <div data-i18n="Account">Apperance Setting</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="{{ route('settings.mail') }}" class="menu-link">
+                            <div data-i18n="Account">Mail Setting</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="{{ route('settings.socialite') }}" class="menu-link">
+                            <div data-i18n="Account">Socialite Setting</div>
+                        </a>
+                    </li>
+                </ul>
+            </li>
         @endcan
     </ul>
 </aside>
